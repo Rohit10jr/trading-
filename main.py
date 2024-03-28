74a29f713185b781845c6cf1e4e985d58c71bc36
Rohit10jr <rohitjworkspace@gmail.com>
2024-03-28 23:15:14 +0530

    charts

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ data = yf.download(ticker, start='2024-01-01', end='2024-02-02')
 print(data)
 
 mpf.plot(data)
+
+mpf.plot(data, type='candle')
+
+data = yf.download(ticker, start='2024-01-01', end='2024-02-02')
+mpf.plot(data, type='renko')
+mpf.plot(data, type='pnf')
